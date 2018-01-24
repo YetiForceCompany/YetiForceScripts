d7ac8e2b9358e4a7e3af03c53a2869bd8c834ff4
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-01-24 11:34:18 +0100

    Add password reset

---
+++ PasswordReset/PasswordReset.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Password reset
+ * @package YetiForce
+ * @copyright YetiForce Sp. z o.o.
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ */
+chdir(__DIR__ . '/../');
+define('IS_PUBLIC_DIR', true);
+require 'include/main/WebUI.php';
+
+$userId = 1;
+$userName = '';
+
+if ($userName) {
+	$userId = \App\User::getUserIdByName($userName);
+}
+$password = \App\Encryption::generateUserPassword();
+$userRecordModel = Users_Record_Model::getInstanceById($userId, 'Users');
+$userRecordModel->set('changeUserPassword', true);
+$userRecordModel->set('user_password', $password);
+$userRecordModel->set('date_password_change', date('Y-m-d H:i:s'));
+$userRecordModel->set('force_password_change', 0);
+$userRecordModel->save();
+
+echo 'User name: ' . $userRecordModel->get('user_name') . ' | Password: ' . $password;
---
+++ PasswordReset/README.md
@@ -0,0 +1,4 @@
+@author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+@description: Password reset of any user
+@install: Copy to the CRM_ROOT/public_html directory
+@run: Set variable $userId or $userName and run the script
