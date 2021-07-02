68989dfbb3611b28ca63a12ba2f3cf9f42b0a674
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-07-02 13:12:46 +0200

    PasswordReset/PasswordReset.php

--- PasswordReset/PasswordReset.php
+++ PasswordReset/PasswordReset.php
@@ -1,10 +1,13 @@
 <?php
 /**
- * Password reset
+ * Password reset.
+ *
  * @package   YetiForce
+ *
  * @copyright YetiForce Sp. z o.o.
  * @license   YetiForce Public License 3.0 (yetiforce.com)
  * @author    Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
+ *
  * @version   >= 4.3.0
  */
 if (file_exists('include/main/WebUI.php')) {
@@ -21,24 +24,25 @@ if (file_exists('include/main/WebUI.php')) {
 	}
 }
 
-
 $userId = 1;
 $userName = '';
 $password = '';
 
-
-
-if ($userName) {
-	$userId = \App\User::getUserIdByName($userName);
+try {
+	if ($userName) {
+		$userId = \App\User::getUserIdByName($userName);
+	}
+	if (!$password) {
+		$password = \App\Encryption::generateUserPassword();
+	}
+	$userRecordModel = Users_Record_Model::getInstanceById($userId, 'Users');
+	$userRecordModel->set('changeUserPassword', true);
+	$userRecordModel->set('user_password', $password);
+	$userRecordModel->set('date_password_change', date('Y-m-d H:i:s'));
+	$userRecordModel->set('force_password_change', 0);
+	$userRecordModel->save();
+} catch (\Throwable $th) {
+	echo $th->__toString();
 }
-if (!$password) {
-	$password = \App\Encryption::generateUserPassword();
-}					
-$userRecordModel = Users_Record_Model::getInstanceById($userId, 'Users');
-$userRecordModel->set('changeUserPassword', true);
-$userRecordModel->set('user_password', $password);
-$userRecordModel->set('date_password_change', date('Y-m-d H:i:s'));
-$userRecordModel->set('force_password_change', 0);
-$userRecordModel->save();
 
 echo 'Login: ' . $userRecordModel->get('user_name') . '<br>Password: ' . $password . '<br>Full name: ' . $userRecordModel->getName();
