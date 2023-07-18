f468cd5ef80d41852a9d8969dc50e99eea24e25c
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2023-07-18 23:43:28 +0200

    YetiREST/index.php

--- YetiREST/index.php
+++ YetiREST/index.php
@@ -9,7 +9,7 @@
  * @version 1.2
  */
 require __DIR__ . '/vendor/autoload.php';
-//disable output buffering
+// disable output buffering
 ob_implicit_flush();
 echo '<pre>';
 /*
@@ -40,50 +40,48 @@ try {
 			print_r($api->listModules());
 
 			// Show record details
-			//print_r($api->getRecord('OfficeLocations', 355));
+			print_r($api->getRecord('OfficeLocations', 355));
 
 			// Show records list
-			//print_r($api->listRecords('OfficeLocations', []));
+			print_r($api->listRecords('OfficeLocations', []));
+
+			// Create new record
+			print_r($api->createRecord('OfficeLocations', [
+				'name' => 'xxx',
+				'ulica' => 'yyy',
+				'miasto' => 'rrr',
+			]));
 
 			// Get fields in module.
-			//print_r($api->fields('OfficeLocations', ['response' => ['inventory', 'blocks', 'privileges', 'dbStructure', 'queryOperators']]));
+			print_r($api->fields('OfficeLocations', ['response' => ['inventory', 'blocks', 'privileges', 'dbStructure', 'queryOperators']]));
 
-			/*
-						// Create new record
-						print_r($api->createRecord('OfficeLocations', [
-							'name' => 'xxx',
-							'ulica' => 'yyy',
-							'miasto' => 'rrr',
-						]));
+			// Create new record
+			print_r($api->createRecord('Contacts', [
+				'firstname' => 'xxx',
+				'lastname' => 'yyy',
+			]));
 
-						// Create new record
-						print_r($api->createRecord('Contacts', [
-							'firstname' => 'xxx',
-							'lastname' => 'yyy',
-						]));
+			// Update exist record
+			print_r($api->updateRecord('Contacts', 355, [
+				'firstname' => 'xxx',
+				'lastname' => 'yy222y',
+			]));
 
-						// Update exist record
-						print_r($api->updateRecord('Contacts', 355, [
-							'firstname' => 'xxx',
-							'lastname' => 'yy222y',
-						]));
+			// Get related modules.
+			print_r($api->relatedModules('Contacts', 355));
 
-						// Get related modules.
-						print_r($api->relatedModules('Contacts', 355));
+			// List related records.
+			print_r($api->listRelatedRecords('Contacts', 355, 'Project', [
+				'rawData' => 1,
+				'limit' => 5,
+			]));
 
-						// List related records.
-						print_r($api->listRelatedRecords('Contacts', 355, 'Project', [
-							'rawData' => 1,
-							'limit' => 5,
-						]));
+			// Delete record.
+			print_r($api->deleteRecord('Contacts', 355));
 
+			// Get record history.
+			print_r($api->recordHistory('Contacts', 355));
 
-						// Delete record.
-						print_r($api->deleteRecord('Contacts', 355));
-
-						// Get record history.
-						print_r($api->recordHistory('Contacts', 355));
-			*/
 			$api->logout();
 		}
 	} catch (\Throwable $th) {
